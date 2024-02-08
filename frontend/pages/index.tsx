3e47c1115241f3cd47cc79e14c37eaf19d96d2f0
Luke Waehner <luke.waehner@gmail.com>
2024-02-08 16:05:35 -0500

    updates

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,13 @@
+// pages/index.tsx
+import React from "react";
+import component from "../components/MyComponent"; // Import your component
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Bussing News</h1>
+    </div>
+  );
+};
+
+export default Home;
